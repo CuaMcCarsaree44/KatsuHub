bb35a0bb6280f6ad154d7e1b4d9798cf12df98b6
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-04-16 10:23:18 +0700

    Created Repository

---
+++ app/src/main/java/com/cua/katsuhub/repository/HistoryRepository.kt
@@ -0,0 +1,23 @@
+package com.cua.katsuhub.repository
+
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.MutableLiveData
+import com.cua.katsuhub.model.room_package.Anime
+import com.cua.katsuhub.room_service.AnimeDao
+
+//TODO 2.7 Create Repository for History
+class HistoryRepository(private val dao:AnimeDao) {
+    // Place to temp all datas from dao
+    val allData: MutableLiveData<List<Anime>> = dao.getAllData()
+
+    /*TODO 2.8 - Since this is non-UI operation, you need a wrapper for this function or app will creash
+        *@WorkerThread is an annotation to tell that this is non-UI operation
+         *Suspend is a modifier to tell this function called from coroutine or another suspending function*/
+    @WorkerThread
+    suspend fun Insert(history:Anime)
+    {
+        dao.insert(history)
+    }
+
+
+}
