adc95c7b757b858e86e538c99cdd7bed6e161457
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-04-16 09:56:05 +0700

    Added Singleton for Room Database

--- app/src/main/java/com/cua/katsuhub/room_service/AnimeRoomDatabase.kt
+++ app/src/main/java/com/cua/katsuhub/room_service/AnimeRoomDatabase.kt
@@ -1,17 +1,41 @@
 package com.cua.katsuhub.room_service
 
-import androidx.room.Database
-import androidx.room.DatabaseConfiguration
-import androidx.room.InvalidationTracker
-import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.room.*
 import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.cua.katsuhub.model.room_package.Anime
 
 //TODO 2.4 - Create a RoomDatabase by making it Abstract class and inherit RoomDatabase()
 @Database(entities = [Anime::class], version = 1) //-> This thing will create Table
-public abstract class AnimeRoomDatabase : RoomDatabase() {
+abstract class AnimeRoomDatabase : RoomDatabase() {
     //TODO 2.5 - Define DAO that works with Database
     abstract fun animeDao():AnimeDao
+
+    //TODO 2.6 - Create AnimeRoomDatabase Singleton to prevent RoomDatabase creating more than 1 instance=
+    companion object{
+        @Volatile
+        private var instance:AnimeRoomDatabase? = null
+
+        fun getDatabase(context: Context):AnimeRoomDatabase{
+            if(instance != null)
+            {
+                return instance as AnimeRoomDatabase
+            }
+
+            synchronized(this){
+                //This is where you put Database Name.
+                val new_instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AnimeRoomDatabase::class.java,
+                    "Katsu_Database").build()
+
+                instance = new_instance
+                return instance as AnimeRoomDatabase
+            }
+        }
+    }
+
+
 
     override fun createOpenHelper(config: DatabaseConfiguration?): SupportSQLiteOpenHelper {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
