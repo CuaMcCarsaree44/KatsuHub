e8f1758191b55efd1b7c832acabd3f179e812075
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-04-22 10:27:37 +0700

    Changed Call<T> into Observable<T>

--- app/src/main/java/com/cua/katsuhub/services/ApiServices.kt
+++ app/src/main/java/com/cua/katsuhub/services/ApiServices.kt
@@ -3,20 +3,21 @@ package com.cua.katsuhub.services
 import com.cua.katsuhub.model.anime.AnimeRoot
 import com.cua.katsuhub.model.animes.Response
 import com.cua.katsuhub.model.response.CharacterResponse
+import io.reactivex.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
-
+//TODO RxJava 101 - 1.2 Change Call into Observable
 interface ApiServices {
     @GET("characters")
-    fun getData(@Query("filter[name]") character: String): Call<CharacterResponse>
+    fun getData(@Query("filter[name]") character: String): Observable<CharacterResponse>
 
     @GET("trending/anime")
-    fun getTrending(): Call<Response>
+    fun getTrending(): Observable<Response>
 
     @GET("anime/{id}")
-    fun getSpecific(@Path("id") id: Int):Call<AnimeRoot>
+    fun getSpecific(@Path("id") id: Int):Observable<AnimeRoot>
 
     @GET("/library-entries")
     fun getTitle(@Query("filter[title]") title: String): Call<Response>
