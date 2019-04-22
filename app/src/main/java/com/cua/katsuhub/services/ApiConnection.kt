f3916530292edfe0c4a65327d7ea71019eace925
CuaMcCarsaree44 <yosuakristianto754@gmail.com>
2019-04-22 10:51:52 +0700

    Make a change in ApiConnection

--- app/src/main/java/com/cua/katsuhub/services/ApiConnection.kt
+++ app/src/main/java/com/cua/katsuhub/services/ApiConnection.kt
@@ -3,9 +3,10 @@ package com.cua.katsuhub.services
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
-
+//TODO RxJava 101 - 1.3 Change everything inside here.
 class ApiConnection {
     private val loggingInterceptor: HttpLoggingInterceptor by lazy {
         HttpLoggingInterceptor()
@@ -28,19 +29,16 @@ class ApiConnection {
             .build()
     }
 
-    private fun setInit(): Retrofit
-    {
-        return Retrofit.Builder()
+    fun create(): ApiServices{
+        val retrofitService = Retrofit.Builder()
             .baseUrl(apiBody)
             .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .client(httpClient)
             .build()
+        return retrofitService.create(ApiServices::class.java)
     }
 
-    fun getInstance(): ApiServices
-    {
-        return setInit().create(ApiServices::class.java)
-    }
 
     companion object {
         private const val apiBody = "https://kitsu.io/api/edge/"
